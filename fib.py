fb973858791ad72298ce470031db7c6e2368dd55
rowatk <rowaneatkinson@gmail.com>
2019-10-17 16:22:18 -0500

    Added full functionality to check a list for fib sequence

--- fib.py
+++ fib.py
@@ -1,4 +1,13 @@
-# Functon to find the nth fib number in the sequence
+def fib_check(lst):
+    target = lst[0]
+    counter = 0
+    while target > fib(counter):
+        counter+=1
+    if counter != target:
+        return False
+    return lst == genFib(counter, counter + len(lst))
+
+
 def fib(x):
     if x == 0:
         return 0
@@ -8,4 +17,13 @@ def fib(x):
         return fib(x-1) + fib(x-2)
 
 
+def genFib(start, end):
+    lst = []
+    for i in range(start, end):
+        lst += [fib(i)]
+    return lst
+
+# print(genFib(0,6))
+print(fib_check([0,1,1,2,3,5]))
+    
 
